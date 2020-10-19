7730fd8b1ac3704bfb829119c4283031553132d8
benjaminchukwudi <benjaminugwu25@gmail.com>
2020-10-19 15:31:25 -0400

    initialcommit

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb://localhost:27017/mern2';
+dbPassword3 = 'mongodb+srv://mumu:mumu@cluster0-jhfy2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ server/routes/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Contact
+router.get('/contact', forwardAuthenticated, (req, res) => res.render('contact'));
+
+
+// About us
+router.get('/about', forwardAuthenticated, (req, res) => res.render('about'));
+
+// FAQ
+
+router.get('/help', forwardAuthenticated, (req, res) => res.render('faq'));
+
+// TERMS
+router.get('/term-and-condition', forwardAuthenticated, (req, res) => res.render('terms'));
+
+//PRIVACY POLICY
+router.get('/privacy-policy', forwardAuthenticated, (req, res) => res.render('privacy'));
+
+//REFUND POLICY
+router.get('/refund-policy', forwardAuthenticated, (req, res) => res.render('refund'));
+
+
+router.get('/how-to-buy-bitcoin', forwardAuthenticated, (req, res) => res.render('how'));
+
+router.get('/what-is-bitcoin', forwardAuthenticated, (req, res) => res.render('bitcoin'));
+
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
+router.get('/profile', ensureAuthenticated, (req, res) =>
+  res.render('profile', {
+    user: req.user
+  })
+);
+
+router.get('/return-on-investments', ensureAuthenticated, (req, res) =>
+  res.render('returns', {
+    user: req.user
+  })
+);
+router.get('/history', ensureAuthenticated, (req, res) =>
+  res.render('history', {
+    user: req.user
+  })
+);
+router.get('/invest', ensureAuthenticated, (req, res) =>
+  res.render('invest', {
+    user: req.user
+  })
+);
+
+router.get('/withdrawal', ensureAuthenticated, (req, res) =>
+  res.render('withdrawal', {
+    user: req.user
+  })
+);
+router.get('/analytics', ensureAuthenticated, (req, res) =>
+  res.render('analytics', {
+    user: req.user
+  })
+);
+
+router.get('/settings', ensureAuthenticated, (req, res) =>
+  res.render('settings', {
+    user: req.user
+  })
+);
+router.get('/checkout', ensureAuthenticated, (req, res) =>
+  res.render('checkout', {
+    user: req.user
+  })
+);
+router.get('/bloop', ensureAuthenticated, (req, res) =>
+  res.render('complete', {
+    user: req.user
+  })
+);
+
+
+module.exports = router;
