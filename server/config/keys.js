e632751e1f160067d235a019c5fbc765ed7f103d
benjaminchukwudi <benjaminugwu25@gmail.com>
2020-10-20 00:34:48 -0400

    all

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,5 @@
-dbPassword = 'mongodb://localhost:27017/mern2';
-dbPassword3 = 'mongodb+srv://mumu:mumu@cluster0-jhfy2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
+dbPassword3 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://coinpavier:coinpavier@cluster0.zaucc.mongodb.net/<dbname>?retryWrites=true&w=majority'
 module.exports = {
     mongoURI: dbPassword
 };
